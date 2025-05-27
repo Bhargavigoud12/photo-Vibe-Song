8f8d16567b1f1da98356a75431d6123c1df60454
Giri Bhargavi <bhargavigoud841@gmail.com>
2025-05-27 22:56:07 +0530

    Saving my local changes before merge

--- app.py
+++ app.py
@@ -81,5 +81,12 @@ def index():
 
     return render_template('index.html', photo=photo_url, mood=mood, songs=songs)
 
+import os
+
+@app.route('/')
+def home():
+    return "Hello from Railway!"
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT env var or 5000
+    app.run(host="0.0.0.0", port=port)
